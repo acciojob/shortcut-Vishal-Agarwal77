7b8f6d12638615dd30a5adcfdcbebe66a6b3fba1
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-19 13:52:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function shortcut(s1, s2) {
   // your code here
+	let ans="";
+	if(s1.length>0 && s2.length>0){
+		ans+=s1.charAt(0)+s2.charAt(0);	
+	}
+	return ans;
 }
 
-// Do not change the code below.
+// Do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
